2b261a55a5909f3a7448ee3f8bb3009963f06f56
Brayden Copley <braydenc303@gmail.com>
2019-10-11 17:42:41 -0600

    Minutes years days working

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,8 +2,8 @@ package academy.learnprogramming;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(AreaCalculator.area(5));
-        System.out.println(AreaCalculator.area(5.0, 4.0));
-        System.out.println(AreaCalculator.area(-1));
+        MinutesYearsDays.printYearsAndDays(525600);
+        MinutesYearsDays.printYearsAndDays(1051200);
+        MinutesYearsDays.printYearsAndDays(561600);
     }
 }
--- src/academy/learnprogramming/MinutesYearsDays.java
+++ src/academy/learnprogramming/MinutesYearsDays.java
@@ -1,5 +1,15 @@
 package academy.learnprogramming;
 
 public class MinutesYearsDays {
+
+    public static void printYearsAndDays(long minutes) {
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / 525600;
+            long days = (minutes % 525500) / 1440;
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
     
 }
