befd9e56f4be828057a31e22ed281b58d17795dc
Brayden Copley <braydenc303@gmail.com>
2019-10-11 17:21:42 -0600

    Area calculator working

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,6 +2,8 @@ package academy.learnprogramming;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println(AreaCalculator.area(5));
+        System.out.println(AreaCalculator.area(5.0, 4.0));
+        System.out.println(AreaCalculator.area(-1));
     }
 }
