73e2fae4cdea289ed9871f0a3f97f38b4993ebbd
Brayden Copley <braydenc303@gmail.com>
2019-10-11 20:46:09 -0600

    created files

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,5 +5,16 @@ public class Main {
         MinutesYearsDays.printYearsAndDays(525600);
         MinutesYearsDays.printYearsAndDays(1051200);
         MinutesYearsDays.printYearsAndDays(561600);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(3);
+        NumberInWord.printNumberInWord(4);
+        NumberInWord.printNumberInWord(5);
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(7);
+        NumberInWord.printNumberInWord(8);
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(10);
     }
 }
